4bb8ca8af726e963e770beacbcf7927e23df0815
Nekromant512 <Kolya-doroshenko@github.com>
2022-01-06 11:48:23 +0200

    test

--- web/modules/custom/nekromant512/src/Controller/Nekromant512Controller.php
+++ web/modules/custom/nekromant512/src/Controller/Nekromant512Controller.php
@@ -22,3 +22,4 @@ class Nekromant512Controller extends ControllerBase {
   }
 
 }
+
--- web/modules/custom/nekromant512/src/Form/CatForm.php
+++ web/modules/custom/nekromant512/src/Form/CatForm.php
@@ -4,25 +4,37 @@ namespace Drupal\nekromant512\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\InsertCommand;
 
 /**
  * {@inheridoc}
  */
 class CatForm extends FormBase {
+
   /**
    * {@inheridoc}
    */
   public function buildForm (array $form, FormStateInterface $form_state) {
-    $form['title'] = [
+    $form['name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your cat`s name:'),
       '#descriptin' => $this->t('Name: 2-32 letters.'),
       '#required' => TRUE,
     ];
 
+    $form['message'] = [
+      '#type' => 'markup',
+      '#markup' => '<div class="resulr_message"></div>',
+    ];
+
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Add cat'),
+      '#ajax' => [
+        'callback' => '::ajaxSubmit',
+      ]
     ];
     return $form;
   }
@@ -36,24 +48,46 @@ class CatForm extends FormBase {
   }
 
   /**
-   * {@inheridoc}
+   *
    */
   public function submitForm (array &$form, FormStateInterface $form_state) {
-    $title =$form_state->getValue('title');
-    $this->messenger()->addMessage($this->t('You cat name: %title.', ['%title' => $title]));
 
   }
+  /**
+   * {@inheridoc}
+   */
+  public function ajaxSubmit (array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    $valideName = $this->validateName($form, $form_state);
+    if ($valideName == '<2'){
+      $css = ['border' => '1px solid red'];
+      $message = $this->t('Name must be more 2');
+    } elseif ($valideName == '>32') {
+      $css = ['border' => '1px solid green'];
+      $message = $this->t('Name must be not more 32');
+    } else {
+      $css = ['border' => '1px solid green'];
+      $message = $this->t('Name is valide');
+    }
+    $response->addCommand(new CssCommand('#
+
+  }
+
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if(strlen($form_state->getValue('title')) <2) {
-      $form_state->setErrorByName(
-        'title',
-        $this->t('Error. Name must be at least 2 letter'));
+
+  }
+
+  public fuction validateName(array &$form, FormStateInterface $form_state) {
+    if (strlen($form_state->getValue('title')) < 2) {
+      return '<2';
     }
     elseif (strlen($form_state->getValue('title')) > 32) {
-      $form_state->setErrorByName(
-        'title',
-        $this->t('Error. Name must be no more 32 letter'));
+      return '>32';
     }
+    else {
+      return 'valide';
+    }
+
   }
 
 }
